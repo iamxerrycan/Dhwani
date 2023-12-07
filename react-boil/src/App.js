bc86e43322de36cf5e9d637fbec7a57c5d829c93
rajshish333 <rajshish333@gmail.com>
2023-12-07 21:03:16 +0530

    first

---
+++ react-boil/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Dropdown from './component/dropdown/Dropdownauto'
+import ListBox from './component/dropdown/Listbox';
+import Chart from './component/chart/Chart'
+
+function App() {
+  return (
+    <div>
+      <Chart/>
+      <Dropdown/>
+      <ListBox/>
+    </div>
+  );
+}
+
+export default App;
