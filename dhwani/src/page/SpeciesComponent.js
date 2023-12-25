082abe36b3096f369fc901b68f39a0426bf0a24c
rajshish333 <rajshish333@gmail.com>
2023-12-25 19:01:08 +0530

    dashboard issue not resolve

--- dhwani/src/page/SpeciesComponent.js
+++ dhwani/src/page/SpeciesComponent.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./PlanetComponent.css";
 
 const SpeciesComponent = (props) => {
-  return (
-    <div>SpeciesComponent</div>
-  )
-}
+  const { name, imageSrc, information, soundSrc } = props;
 
-export default SpeciesComponent
+  return (
+    <div className="planet-container">
+      <div className="first-one">
+        <img className="planet-image" src={imageSrc} alt={name} />
+        <p>
+          Name: {name} | Radiation Level: {information}
+        </p>
+      </div>
+      <div className="second-one">
+        <p className="description-text">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+        <audio className="planet-sound" src={soundSrc} controls loop />
+      </div>
+    </div>
+  );
+};
+
+export default SpeciesComponent;
