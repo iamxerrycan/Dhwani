cfa13e8c24ae86e939ca8180a4d98f8efed8fa00
rajshish333 <rajshish333@gmail.com>
2023-12-25 14:02:07 +0530

    dashboard issue not resolve

--- dhwani/src/page/PlanetComponent.js
+++ dhwani/src/page/PlanetComponent.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./PlanetComponent.css";
 
 const PlanetComponent = (props) => {
-  return (
-    <div>PlanetComponent</div>
-  )
-}
+  const { name, imageSrc, radiationLevel, soundSrc } = props;
 
-export default PlanetComponent
+  return (
+    <div className="planet-container">
+      <img className="planet-image" src={imageSrc} alt={name} />
+      <p>Name: {name} | Radiation Level: {radiationLevel}</p>
+      <p className="description-text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </p>
+      <audio className="planet-sound" src={soundSrc} controls loop />
+    </div>
+  );
+};
+
+export default PlanetComponent;
